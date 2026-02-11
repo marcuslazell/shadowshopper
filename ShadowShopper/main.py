2f07c56fa77e24722f47c0804f833618b656d117
Marc Shaw <marcus@marcuslshaw.com>
2026-02-11 04:23:29 -0800

    Login Button

--- ShadowShopper/main.py
+++ ShadowShopper/main.py
@@ -12,6 +12,10 @@ username.send_keys("standard_user")
 password = driver.find_element(By.ID, "password")
 password.send_keys("secret_sauce")
 
+login_button = driver.find_element(By.ID, 'login-button')
+login_button.click
+print("button has been click")
+
 import time
-time.sleep(5)
+time.sleep(20)
 driver.quit()
