20f517c6f402e994c73daf8afc1c768d5ae55db0
Marc Shaw <marcus@marcuslshaw.com>
2026-02-11 04:20:02 -0800

    login credintals

--- ShadowShopper/main.py
+++ ShadowShopper/main.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
 
 # This opens a new Firefox window
 driver = webdriver.Firefox()
 
-driver.get("https://www.google.com")
+driver.get("https://www.saucedemo.com")
+
+username = driver.find_element(By.ID, 'user-name')
+username.send_keys("standard_user")
+password = driver.find_element(By.ID, "password")
+password.send_keys("secret_sauce")
 
 import time
 time.sleep(5)
